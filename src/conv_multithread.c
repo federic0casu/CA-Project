990516c15d5d9276905ce0c1fc726abab5168a4b
federic0casu <federicocasu.mail@gmail.com>
2023-04-02 20:44:51 +0200

    Add 1st implementation of multithread convolution

---
+++ src/conv_multithread.c
@@ -0,0 +1,52 @@
+/*
+ * Multithread implementantion of convolution
+ */ 
+#include <omp.h>
+
+void convolution(int* output, int* input, int* kernel, int input_rows, int input_columns, int kernel_size)
+{
+    // This holds the convolution results for an index.
+    int convolute = 0;
+    
+    // Used for input matrix index.
+    int i, j, x, y, k, l;
+
+    #pragma omp parallel shared(output, input, kernel, input_rows, input_columns, kernel_size) private(convolute, x, y, i, j, k, l) 
+    {
+        #pragma omp for nowait
+        // Fill output matrix: rows and columns are i and j respectively.
+        for (i = 0; i < (input_rows - kernel_size + 1); i++)
+        {
+            for (j = 0; j < (input_columns - kernel_size + 1); j++)
+            {
+                x = i;
+                y = j;
+
+                // Kernel rows and columns are k and l respectively.
+                for (k = 0; k < kernel_size; k++)
+                {
+                    for (l = 0; l < kernel_size; l++)
+                    {
+                        // Convolute here.
+                        convolute += kernel[k*kernel_size + l] * input[x*input_columns + y];
+                        //printf("\tTHREAD[%d] -> Computing kernel[%d][%d]*input[%d][%d] = %d\n", omp_get_thread_num(), k, l, x, y, kernel[k*kernel_size + l]*input[x*input_columns + y]);
+
+                        // Move right.
+                        y++;
+                    }
+                    // Move down.
+                    x++; 
+
+                    // Restart column position
+                    y = j;
+                }
+                // Add result to output matrix.
+                output[i*(input_columns - kernel_size + 1) + j] = convolute;
+                //printf("\tTHREAD[%d] -> output[%d][%d] = %d\n", omp_get_thread_num(), i, j, convolute);
+                
+                // Needed before we move on to the next index.
+                convolute = 0;
+            }
+        }
+    }
+}
--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 make
 for rep in {1..1}
 do 
-    ./"create_input" input.txt 4 4 kernel.txt 3 3 $rep
+    ./"create_input" input.txt 25 25 kernel.txt 3 3 $rep
     ./"conv" input.txt kernel.txt $rep
 done
